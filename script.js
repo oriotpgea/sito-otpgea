a7c2c7844836ec837760660df249457d01f7baca
Riccardo Fava <riccardo.fava1@gmail.com>
2025-09-02 18:24:00 +0200

    Major update - struttura a più pagine

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-// Seleziona tutti i link della navigazione e le sezioni della pagina
-const navLinks = document.querySelectorAll('.nav-links a');
-const sections = document.querySelectorAll('section');
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
 
-// Funzione per cambiare il link attivo
-function changeActiveLink() {
-    let index = sections.length;
-
-    // Scorre le sezioni dal basso verso l'alto
-    while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
-    
-    // Rimuove la classe 'active' da tutti i link
-    navLinks.forEach((link) => link.classList.remove('active'));
-    
-    // Aggiunge la classe 'active' al link corrispondente alla sezione visualizzata
-    // Controlla che il link esista prima di aggiungere la classe
-    if (navLinks[index]) {
-        navLinks[index].classList.add('active');
-    }
-}
-
-// Esegue la funzione al caricamento della pagina e durante lo scorrimento
-window.addEventListener('load', changeActiveLink);
-window.addEventListener('scroll', changeActiveLink);
+    menuToggle.addEventListener('click', function () {
+        navLinks.classList.toggle('active');
+        // Cambia l'icona da hamburger a "X" e viceversa
+        const icon = menuToggle.querySelector('i');
+        if (icon.classList.contains('fa-bars')) {
+            icon.classList.remove('fa-bars');
+            icon.classList.add('fa-times');
+        } else {
+            icon.classList.remove('fa-times');
+            icon.classList.add('fa-bars');
+        }
+    });
+});
